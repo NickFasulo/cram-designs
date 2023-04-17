44e8b47838ce6bc39d6e258e202466d036e6405d
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-04-17 10:00:15 -0400

    remove slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Slider from './components/Slider'
 import StyledNav from './components/StyledNav'
 
 export default function App() {
   return (
     <div className='App'>
       <StyledNav />
-      <br />
-      <Slider />
     </div>
   )
 }
--- src/components/StyledImg.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const ImgStyles = styled.img`
-  max-height: 20rem;
-`
-
-export default function StyledImg({ ...props }) {
-  return <ImgStyles src={props.src} />
-}
