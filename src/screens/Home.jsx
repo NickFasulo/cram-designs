193e0ae260b09d65d2c01801a85cc9594b922b57
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-05-15 21:39:53 -0400

    convert images to webp format

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -32,23 +32,23 @@ export default function Home() {
       <h2>Check out these custom made shirts!</h2>
       <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <Masonry>
-          <img src='/images/design-1.png' />
-          <img src='/images/design-3.png' />
-          <img src='/images/design-4.png' />
-          <img src='/images/design-5.png' />
-          <img src='/images/design-6.png' />
-          <img src='/images/design-7.png' />
-          <img src='/images/design-8.png' />
-          <img src='/images/design-9.png' />
-          <img src='/images/design-10.png' />
-          <img src='/images/design-11.png' />
-          <img src='/images/design-12.png' />
-          <img src='/images/design-13.png' />
-          <img src='/images/design-14.png' />
-          <img src='/images/design-15.png' />
-          <img src='/images/design-16.png' />
-          <img src='/images/design-17.png' />
-          <img src='/images/design-18.png' />
+          <img src='/images/design-1.webp' />
+          <img src='/images/design-3.webp' />
+          <img src='/images/design-4.webp' />
+          <img src='/images/design-5.webp' />
+          <img src='/images/design-6.webp' />
+          <img src='/images/design-7.webp' />
+          <img src='/images/design-8.webp' />
+          <img src='/images/design-9.webp' />
+          <img src='/images/design-10.webp' />
+          <img src='/images/design-11.webp' />
+          <img src='/images/design-12.webp' />
+          <img src='/images/design-13.webp' />
+          <img src='/images/design-14.webp' />
+          <img src='/images/design-15.webp' />
+          <img src='/images/design-16.webp' />
+          <img src='/images/design-17.webp' />
+          <img src='/images/design-18.webp' />
         </Masonry>
       </ResponsiveMasonry>
     </Wrapper>
